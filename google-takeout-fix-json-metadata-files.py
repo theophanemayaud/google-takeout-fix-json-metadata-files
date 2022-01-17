783fbc1fbd737ed674d3010927bf6eb893dbaeb3
Théophane Mayaud <farineux.poilu0h@icloud.com>
2022-01-17 20:46:09 +0100

    chore: creating repo

---
+++ google-takeout-fix-json-metadata-files.py
@@ -0,0 +1,8 @@
+# google-takeout-fix-json-metadata-files
+# created by Théophane Mayaud for use when downloading large libraries
+# from google photos, through / using Google Takeout (takeout.google.com)
+
+# Use this freely, you can simply run it with the first argument being the
+# directory to scan and correct
+
+
